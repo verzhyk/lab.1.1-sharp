6c62ffc06f4ed127d1b7fa9ec671bc34f6b3a319
Влад Верожинский <vladyslav.verozhynskyi.itis.2020@lpnu.ua>
2021-03-02 00:27:42 +0200

    create project

---
+++ FloatPower.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1_1_sharp
+{
+    public class FloatPower
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+
+        public FloatPower(int first, int second)
+        {
+            if (first > 0) First = first;
+            else First = 1;
+            if (second > 0) Second = second;
+            else Second = 1;
+        }
+
+        public int Power()
+        {
+            if (First > 0 && Second > 0)
+            {
+                int result = 1;
+                for (int i = 0; i < Second; i++)
+                {
+                    result *= First;
+                }
+                return result;
+            }
+            else return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1_1_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FloatPower floatPower = new FloatPower(10, 2);
+            Console.WriteLine(floatPower.Power());
+        }
+    }
+}
